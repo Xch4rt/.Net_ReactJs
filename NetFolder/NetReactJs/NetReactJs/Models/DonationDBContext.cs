6a831f7ab1e39cc60808d1c46a19dfe26e57cd8a
Xch4rt <pab203.guti@gmail.com>
2022-08-06 22:32:53 -0600

    Backend asp.net inicio

---
+++ NetFolder/NetReactJs/NetReactJs/Models/DonationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NetReactJs.Models
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<DCandidate> Dcandidates { get; set; }
+    }
+}
